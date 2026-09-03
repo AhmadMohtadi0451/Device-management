93eb61ddb5cb552b4fcb27f49aab162e6bac616b
ahmad <ahmadmohtadi0451@gmail.com>
2026-09-03 14:11:44 +0330

    feat(store): add addDevice function

--- store/deviceStore.ts
+++ store/deviceStore.ts
@@ -4,11 +4,17 @@ import { mockDevices } from "@/lib/mockData";
 
 interface DeviceStore {
   devices: Device[];
+  addDevice: (device: Device) => void;
   removeDevice: (id: string) => void;
 }
 
 const useDeviceStore = create<DeviceStore>((set) => ({
   devices: mockDevices,
+
+  addDevice: (device) =>
+    set((state) => ({
+      devices: [...state.devices, device],
+    })),
 
   removeDevice: (id) =>
     set((state) => ({
