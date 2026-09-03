8f72f9642982798d457af54f41fabde3a4f0d72e
ahmad <ahmadmohtadi0451@gmail.com>
2026-09-03 15:25:24 +0330

    refactor(page):implement loading for suspense

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,13 @@ import Home from "./homeClient";
 
 export default function Page() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense
+      fallback={
+        <div className="min-h-screen bg-linear-to-br from-slate-50 via-white to-blue-50/30 flex items-center justify-center">
+          <div className="inline-block animate-spin rounded-full h-12 w-12 border-4 border-blue-600 border-t-transparent"></div>
+        </div>
+      }
+    >
       <Home />
     </Suspense>
   );
